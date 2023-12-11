a32d2b19ad18ca2d4a1b8ba06e18c3db004d4262
ash <97464181+Borgerr@users.noreply.github.com>
2023-12-10 19:42:49 -0700

    compiler driven development, intellisense wasn't working :skull:

--- src/rom.rs
+++ src/rom.rs
@@ -6,33 +6,33 @@ pub enum RomReadError {
 pub struct ROM {
     // https://www.nesdev.org/wiki/INES
     header: [u8; 16],
-    trainer: Option([u8; 512]),
-    prg_rom: [u8], // 16384 * x bytes
-    chr_rom: [u8], // 8192 * y bytes
-    inst_rom: Option([u8; 8192]),
-    prom: Option([u8; 16]), // often missing
+    trainer: Option<[u8; 512]>,
+    prg_rom: Vec<u8>, // 16384 * x bytes
+    chr_rom: Vec<u8>, // 8192 * y bytes
+    inst_rom: Option<[u8; 8192]>,
+    prom: Option<[u8; 16]>, // often missing
 }
 
 impl ROM {
-    pub fn new(filebytes: Vec<u8>) -> Result<this, RomReadError> {
+    pub fn new(filebytes: Vec<u8>) -> Result<ROM, RomReadError> {
         if filebytes.len() < 16 {
-            return err(RomReadError::TooShort);
+            return Err(RomReadError::TooShort);
         }
-        let header: [u8; 16] = filebytes[0..16];
+        let header = &filebytes[0..16];
 
         // ensure header bytes are valid
-        let valid_first_three: [u8; 4] = [0x4e, 0x45, 0x53, 0x1a];
-        for i in 0..4 {
-            if header[i] != valid_first_three[i] {
-                return err(RomReadError::InvalidHeader(i));
+        let valid_first_three = [0x4e, 0x45, 0x53, 0x1a];
+        for index in 0..4 {
+            if header[index] != valid_first_three[index] {
+                return Err(RomReadError::InvalidHeader { index });
             }
         }
 
         let prg_rom_size = header[4];
-        let prg_rom = [u8; prg_rom_size];
+        let prg_rom = Vec::with_capacity(prg_rom_size as usize);
 
         let chr_rom_size = header[5];
-        let chr_rom = [u8; chr_rom_size];
+        let chr_rom = Vec::with_capacity(chr_rom_size as usize);
 
         // TODO: implement flags and unused padding
         todo!()
